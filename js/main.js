f097ed32359465d3df170a8e640c0ed917fc6889
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-06-10 02:26:52 +0300

    kjs

---
+++ js/main.js
@@ -0,0 +1,86 @@
+// {
+//         // 1 - masala
+//     function perimetr() {
+//       let a =  +prompt("kvadratning a tomonini kiriting va perimetri aniqlanib console da chiqarilsin")
+//         return 4 * a
+    
+//     }
+    
+//     console.log(perimetr());
+
+// }
+
+
+// {
+//         // 2-masala
+//     function yuza() {
+//         let a = +prompt("Kvadratning a tomonini kiriting va uning yuzasi topilsin, chiqqan natija consoleda paydo bo'lsin")
+//         return a * a
+//     }
+    
+//     console.log(yuza());
+
+// }
+// 
+// {
+//     // 4 - masala
+//     alert("Aylananing diametri kiritilganda uning uzunligi topilsin")
+//     let d = +prompt("aylananing diametrini kiriting")
+//     let p = 3.14
+//     function uzunligi() {
+//         return p * d
+//     }
+//     console.log(uzunligi());
+// }
+// {
+//     // 5 - masala
+//     alert("Kubning yon tomoni (a) kiritilganda uning hajmi va to'la sirti topilsin")
+//     let a = +prompt("a tomonini kiriting")
+//     function hajmi() {
+//         return a * a * a
+//     }
+//     console.log(hajmi());
+//     function tolaSirt() {
+//         return 6 * a * a
+//     }
+//     console.log(tolaSirt());
+// }
+// {
+//     // 6 - masala
+//     alert("paralelepipedning tomonlarini kiritganda uning hajmi va to'la sirti topilsin")
+//     let a = +prompt("a tomonini kiriting")
+//     let b = +prompt("b tomonini kiriting")
+//     let c = +prompt("c tomonini kiriting")
+//     function hajmi() {
+//         return a * b * c
+//     }
+//     console.log(hajmi());
+//     function tolaSirt() {
+//         return 2 * ( a * b + b * c + a * c)
+//     }
+//     console.log(tolaSirt);
+// }
+// {
+//     // 7 - masala
+//     alert(" doiraning radiusini kiritganda uning uzunligi va yuzasi aniqlansin")
+//     let p = 3.14
+//     let r = +prompt("doira radiusini kiriting (r)")
+//     function uzunligi() {
+//         return 2 * p * r
+//     }
+//     console.log(uzunligi());
+//     function yuzasi() {
+//         return p * r * r
+//     }
+//     console.log(yuzasi());
+// }
+// {
+//     // 8 - masala 
+//     alert("ikkita son kiritganda ularni o'rta arifmetigi topilsin")
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     function ortaArifmetik() {
+//         return (a + b) / 2
+//     }
+//     console.log(ortaArifmetik());
+// }
